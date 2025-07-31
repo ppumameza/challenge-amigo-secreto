c4279072adb8a03681df73f08495a2e21f4d83dc
ppumameza <ppuma2603@gmail.com>
2025-07-30 22:02:07 -0500

    Se agrega función agregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+
+function agregarAmigo(){
+    let textoAmigo = document.getElementById('amigo');
+    let nombre = textoAmigo.value.trim();
+
+    if(nombre !==''){
+        amigos.push(nombre);//agregar a la lista
+        mostrarAmigos();//Actualizar la lista en pantalla
+        textoAmigo.value = "";//Limpia el campo texto
+    }else{
+        alert('Por favor escriba el nombre de un amigo');
+    }
+}
